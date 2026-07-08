7d8d71f8e4cabaffbb9aeff343515033324e17fb
Aryan-Koundal <aryankoundal3039@gmail.com>
2026-07-09 00:31:28 +0530

    initial commit

---
+++ index.py
@@ -0,0 +1,20 @@
+print("Hello, World!")
+name ="aryan"
+age = 18
+print (" My name is", name, "and I am", age, "years old.")
+print ("I am learning Python programming.")
+
+if(age >= 18) :
+       print("you can vote")
+else :
+    if(age<12):
+       print("you'r too samll to vote ")
+
+print("my name is : ", name )
+print ("my age is :", age )
+print (type ( name))
+print (type (age))
+a = 19
+b=20
+sum = a+b
+print (sum)
