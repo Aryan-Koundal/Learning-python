f7da123459f5282177188795e23dfef069176242
Aryan-Koundal <aryankoundal3039@gmail.com>
2026-07-19 21:11:04 +0530

    oops in python

--- dectionary.py
+++ dectionary.py
@@ -1,32 +1,35 @@
-student = {
-    "name" : input("entre your name"),
-    "class" : input ("entre your class"),
-    "roll no." :input ("entre your roll no."),
-    "marks" : []
-}
-subject = ["maths","english","hindi","dbms","eco"]
-for i in range(5) :
-    mark = int(input(f"enter marks of {subject[i]} :"))
-    student["marks"].append(mark)
+# student = {
+#     "name" : input("entre your name"),
+#     "class" : input ("entre your class"),
+#     "roll no." :input ("entre your roll no."),
+#     "marks" : []
+# }
+# subject = ["maths","english","hindi","dbms","eco"]
+# for i in range(5) :
+#     mark = int(input(f"enter marks of {subject[i]} :"))
+#     student["marks"].append(mark)
 
-print("Marks :", student["marks"])
+# print("Marks :", student["marks"])
 
-student["name"]="aryan"
-print (student["name"])
-null =  {}
-print (null)
 
-student = {
-    "name ": input("entre your name"),
-    "class": input ("entre your class"),
-    "roll no." : input ("entre your roll no."),
-    "subjest": {
-        "english": input ("entre marks of english"),
-        "maths": input ("entre marks of math"),
-        "econimics": input ("entre marks of economics")
-    }
-}
-print (student["subjest"]["english"])
+
+# student["name"]="aryan"
+# print (student["name"])
+# null =  {}
+# print (null)
+
+# student = {
+#     "name ": input("entre your name"),
+#     "class": input ("entre your class"),
+#     "roll no." : input ("entre your roll no."),
+#     "subjest": {
+#         "english": input ("entre marks of english"),
+#         "maths": input ("entre marks of math"),
+#         "econimics": input ("entre marks of economics")
+#     }
+# }
+# print (student["subjest"]["english"])
+
 student = {
     "name ": "aryan" ,
     "class": 1,
@@ -37,11 +40,11 @@ student = {
         "econimics":88
     }
 }
-print (len (list(student.keys())))
-print (student.values())
-print (student.items())
-pairs = list(student.items())
-print (pairs[1])
+# print (len (list(student.keys())))
+# print (student.values())
+# print (student.items())
+# pairs = list(student.items())
+# print (pairs[1])
 print (student.get("name"))
-student.update({"age":18})
-print (student)
+# student.update({"age":18})
+# print (student)
--- oop in python.py
+++ oop in python.py
@@ -11,7 +11,7 @@ class Student :
         print ("you got",self.marks,"marks")
 
 s1 = Student("aryan",97)
-s1.hello()
+s1.hello() 
 s1.mark()
 print ("from ",Student.college_name)
 s2 = Student("kaku",97)
@@ -123,4 +123,151 @@ class Account :
 acc1 = Account(10000,"aryan123")
 acc1.debit(100)
 acc1.credit(10000)
+  
 
+#del keyword
+class Student :
+    def __init__(self,name):
+        self.name = name 
+
+s1 = Student("kaku")
+print (s1.name)
+del s1.name
+print (s1.name)
+
+#private and public attribute
+class Acc :
+    def __init__(self,acc_no,acc_pass):
+        self.acc_no = acc_no
+        self.__acc_pass = acc_pass
+
+    def reset_pass(self):
+        print (self.__acc_pass)
+
+acc1 = Acc("123","321")
+print (acc1.acc_no)
+print (acc1.reset_pass())
+
+class Person:
+    __name = "Aryan"
+
+    def __hello(self):
+        print ("hello user")
+
+    def Welcome (self):
+        self.__hello()
+
+p1 = Person ()
+print (p1.Welcome())
+
+# Inheritance
+#single inheritance
+class Car :
+    @staticmethod
+    def start ():
+        print ("car started")
+        
+
+    @staticmethod
+    def stop ():
+        print ("car stoped")
+        
+
+class Toyota(Car):
+    def __init__(self,name):
+        self.name = name 
+car1 = Toyota("fortuner")
+car2 = Toyota("prius")
+print (car1.name)
+car1.start()
+car1.stop()
+print (car2.name)
+car2.start ()
+car2.stop()
+
+#multi level inheritance
+class Car :
+    @staticmethod
+    def start ():
+        print ("car started")
+        
+
+    @staticmethod
+    def stop ():
+        print ("car stoped")
+        
+
+class Toyota(Car):
+    def __init__(self,name):
+        self.name = name
+ 
+class Fortuner(Toyota):
+    def __init__(self,type):
+        self.type = type
+
+car1 = Toyota("Fortuner")
+print (car1.name)
+car1 = Fortuner("diesel")
+print ("type",car1.type)
+car1.start()
+
+
+#multiple inheritance
+
+class A :
+    a = "welcome to class A"
+
+class B :
+    b = "welcome to class B"
+
+class C (A,B):
+    c = "welcome to class c"
+
+c1 = C()
+print (c1.c)
+print (c1.b)
+print (c1.a)
+
+
+# super method 
+class Car :
+    def __init__(self,type):
+      self.type = type
+    @staticmethod
+    def start ():
+       print ("car started...")
+    @staticmethod
+    def stop():
+       print ("car stopped.")
+class Toyota(Car):
+    def __init__(self,name, type):
+        self.name = name
+        super().__init__(type)
+        super().start()
+
+car1 = Toyota("fortuner","electric")
+print (car1.type)
+   
+
+#class method 
+class Student :
+    name = "aryan"
+    @classmethod
+    def changename (cls, name ):
+        cls.name = name
+p1 = Student()
+p1.changename("kaku")
+print (p1.name)
+print (Student.name)
+
+#another method 
+class Student :
+    name = "aryan"
+    
+    def changename (self , name ):
+        self.__class__.name = "kaku"
+        Student.name = name
+p1 = Student()
+p1.changename("kaku")
+print (p1.name)
+print (Student.name)
